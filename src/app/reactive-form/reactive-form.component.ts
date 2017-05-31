55c64f9037e99c3762144b4cefde358c8f31bfd4
gritman <gritman@126.com>
2017-05-31 21:35:23 +0800

    响应式表单用到的类

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormArray, FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -6,6 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reactive-form.component.css']
 })
 export class ReactiveFormComponent implements OnInit {
+
+  // 模板式表单的ngModel指令会为input标签自动创建一个FormControl指令
+  username: FormControl = new FormControl('aaa'); // 参数是初始值
+
+  // 代表整个表单,或者表单字段的固定的子集
+  formModel: FormGroup = new FormGroup({
+    from: new FormControl(),
+    to: new FormControl()
+  });
+
+  // 代表可以增长的字段集合,比如工作经历,获奖经历
+  // 没有key只有序号
+  emails: FormArray = new FormArray([
+    new FormControl('a@a.com'),
+    new FormControl('b@b.com')
+  ]);
 
   constructor() { }
 
